a74b132455393eb4392478663304a38b5f604221
yuftlens1 <1606059673yft@gmail.com>
2020-11-12 23:18:02 +0800

    ssh脚本函数版

--- 函数互相调用.py
+++ 函数互相调用.py
@@ -6,17 +6,17 @@ def test2():
     test1()           #调用函数test1。
     test1()
     print('这是test2 #end')
-#test2()
+test2()
 
-def fgx():
-    print('--' * 40)
-
-
-def fgxs(num):
-    i = 0
-    while i < num:
-        fgx()          #调用函数
-        i += 1
-
-fgxs(5)
+# def fgx():
+#     print('--' * 40)
+#
+#
+# def fgxs(num):
+#     i = 0
+#     while i < num:
+#         fgx()          #调用函数
+#         i += 1
+#
+# fgxs(5)
 
